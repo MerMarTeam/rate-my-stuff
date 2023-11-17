66a9843c107e68a46683056cb15d6183c7943218
Mert Toprak <mert.tprk@gmail.com>
2023-11-17 12:00:31 +0100

    added API connection to HomePage & Display posts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function HomePage() {
+	const [postsArray, setPostsArray] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://bootcamp-json-server-backend.adaptable.app/posts')
+			.then((response) => {
+				console.log('Connection to API success.....!');
+				setPostsArray(response.data);
+			})
+			.catch((error) => {
+				console.log('Connection Failed' + '  ' + error);
+			});
+	}, []);
 	return (
 		<div>
-			<h1>Title of the Post</h1>
+			<h1>Posts</h1>
+			{postsArray.map((post) => {
+				return (
+					<div className="post" key={post.id}>
+						<h1>{post.title}</h1>
+					</div>
+				);
+			})}
 			<button>
 				<a href="/create">Create</a>
 			</button>
