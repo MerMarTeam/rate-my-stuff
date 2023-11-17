ccabe085552b2247b3a9161a8afaaaf5b6128b35
pomerama <marpom@gmail.com>
2023-11-17 12:36:48 +0100

    added a very basic Navbar with link to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import HomePage from './pages/HomePage';
 import CreatePost from './pages/CreatePost';
 import DetailPost from './pages/DetailPost';
 import EditPost from './pages/EditPost';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/create" element={<CreatePost />} />
