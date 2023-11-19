7095b886db7db75b81972bd3a810c1fa2df0d013
pomerama <marpom@gmail.com>
2023-11-19 09:04:58 +0100

    basic functional register and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
@@ -6,7 +5,8 @@ import CreatePost from './pages/CreatePost';
 import DetailPost from './pages/DetailPost';
 import EditPost from './pages/EditPost';
 import Navbar from './components/Navbar';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
 
 function App() {
 	return (
@@ -14,9 +14,12 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+
 				<Route path="/create" element={<CreatePost />} />
 				<Route path="/posts/:postId" element={<DetailPost />} />
 				<Route path="/edit/:postId" element={<EditPost />} />
+
+				<Route path="/register" element={<RegisterPage />} />
 				<Route path="/login" element={<LoginPage />} />
 			</Routes>
 		</>
---
+++ src/pages/auth/LoginPage.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import api from './../../api'
+
+function LoginPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleFormSubmit(event) {
+        event.preventDefault()
+
+        let newUser = {
+            email: email,
+            password: password,
+        };
+
+        api
+            .post('/login', newUser)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log('Connection Failed' + '  ' + error);
+            });
+
+        // navigate('/');
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="email-input">Email: </label>
+                <input type="text" id="email-input" required value={email} onChange={event => setEmail(event.target.value)} />
+
+                <label htmlFor="password-input">Password: </label>
+                <input type="password" id="password-input" required value={password} onChange={event => setPassword(event.target.value)} />
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage;
