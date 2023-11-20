466266f95fcd1679558793ec08aeaff5058d27f6
pomerama <marpom@gmail.com>
2023-11-20 16:49:07 +0100

    rating component nearly works. to fix: very first rating is not displayed as average

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,21 +2,37 @@ import { useEffect, useState } from "react";
 import api from './../api'
 
 function Rating(props) {
-    let [givenRating, setGivenRating] = useState()
-    let [post, setPost] = useState()
-    let [postRating, setPostRating] = useState([])
+    let [givenRating, setGivenRating] = useState(0)
 
-    useEffect(() => {
-        getDataFromApi();
-    }, []);
+    let [postRatings, setPostRatings] = useState([])
+
+    let [averageRating, setAverageRating] = useState(0)
+
+    function calculateAverageRating() {
+        let sum = 0;
+        
+        if (givenRating > 0) sum = givenRating;
+
+        for (let i = 0; i < postRatings.length; i++) {
+            sum += postRatings[i].starsCount
+        }
+
+        let average;
+        // If the user provided a new rating, we shall divide by one more
+        if (givenRating > 0) {
+            average = Math.ceil(sum / (postRatings.length + 1));
+        } else {
+            average = Math.ceil(sum / postRatings.length)
+        }
+        setAverageRating(average)
+    }
 
     function getDataFromApi() {
         api
-            .get(`/posts/${props.postId}`)
+            .get(`/posts/${props.postId}/ratings`)
             .then((response) => {
                 console.log('API: getting posts success');
-                setPost(response.data);
-                if (response.data.rating) setPostRating(response.data.rating);
+                setPostRatings(response.data);
 
             })
             .catch((error) => {
@@ -24,15 +40,16 @@ function Rating(props) {
             });
     }
 
-    function putDataToApi(newRatingArray) {
-        let updatePost = {
-            title: `${post.title}`,
-            image: `${post.image}`,
-            rating: newRatingArray
+    function putDataToApi(newRatingNumber) {
+        let newRatingObject = {
+            // TODO when user auth works
+            // "userId": 1,
+            starsCount: newRatingNumber,
+            postId: props.postId,
         };
 
         api
-            .put(`/posts/${props.postId}`, updatePost)
+            .post(`/posts/${props.postId}/ratings`, newRatingObject)
             .then((response) => {
                 console.log('API: putting new rating on a post');
             })
@@ -42,28 +59,40 @@ function Rating(props) {
     }
 
     function handleStarClick(event) {
-
-        let newRating = [...postRating];
-
+        getDataFromApi();
+        let userRating = 0;
         for (let i = 1; i <= 5; i++) {
-            if (event.target.id == `rating-${i}`) newRating.push(i);
+            if (event.target.id == `rating-${i}`) userRating = i;
         }
-
-        putDataToApi(newRating)
-
-
-        // change the style of the stars
+        putDataToApi(userRating);
+        setGivenRating(userRating);
+        // TODO: change the style of the stars depending on which rating button is clicked
     }
 
+    useEffect(() => {
+        getDataFromApi();
+    }, []);
+
+    useEffect(() => {
+        if (postRatings.length) {
+            calculateAverageRating();
+        }
+    }, [postRatings, givenRating]);
+
+
     return (
+
         <div className='post-rating'>
-            {postRating && <p>Rating: {postRating}</p>}
+            <p>Average rating: {
+                averageRating == 0 ? "no rating yet" : averageRating
+            }</p>
             <button id="rating-1" onClick={handleStarClick}>1</button>
             <button id="rating-2" onClick={handleStarClick}>2</button>
             <button id="rating-3" onClick={handleStarClick}>3</button>
             <button id="rating-4" onClick={handleStarClick}>4</button>
             <button id="rating-5" onClick={handleStarClick}>5</button>
-            <p>Your rating: {givenRating}</p>
+            <p>Given rating: {givenRating}</p>
+            <button onClick={calculateAverageRating}>CALCULATE</button>
         </div>
     )
 }
