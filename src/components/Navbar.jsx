16affafa11ea91f1b0449bcd8a85e20d683927da
Mert Toprak <mert.tprk@gmail.com>
2023-11-22 16:37:37 +0100

    added link to the spinning star and edited logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from '../images/logo.png';
 
 function Navbar(props) {
 	const [userEmail, setUserEmail] = useState();
@@ -12,29 +12,32 @@ function Navbar(props) {
 
 	useEffect(() => {
 		getDataFromLocalStorage();
-
 	}, []);
 
 	function handleLogout(event) {
 		localStorage.removeItem('accessToken');
 		localStorage.removeItem('userEmail');
 		localStorage.removeItem('userId');
-		console.log("User was logged out.")
+		console.log('User was logged out.');
 		window.location.reload();
 	}
 
 	return (
 		<nav className="navbar">
-			<i class="fa-solid fa-star fa-spin fa-2xl"></i>
 			<Link to="/">
-				<img className="logo" src="../src/images/logo.png" alt="" />
+				<i class="fa-solid fa-star fa-spin fa-2xl"></i>
+
+				<img className="logo" src={logo} alt="" />
 			</Link>
 
-			<button><Link to="/register">Register</Link></button>
-			<button><Link to="/login">Login</Link></button>
+			<button>
+				<Link to="/register">Register</Link>
+			</button>
+			<button>
+				<Link to="/login">Login</Link>
+			</button>
 			<button onClick={handleLogout}>Logout</button>
 			{userEmail && <p>User email: {userEmail}</p>}
-
 		</nav>
 	);
 }
