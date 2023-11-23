f8745422e11b78a702aacad5751edc02a62cee6a
pomerama <marpom@gmail.com>
2023-11-23 20:46:32 +0100

    reverted navbar to non-bootstrap version, added margin bot and a hr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,12 +23,12 @@ function Navbar(props) {
 	}
 
 	return (
-		<nav className="navbar">
+		<nav>
 			<Link to="/">
 				<i className="fa-solid fa-star fa-spin fa-2xl" style={{ color: "white", marginRight: '20px', paddingRight: '10px' }}></i>
-
 				<img className="logo" src={logo} alt="" />
 			</Link>
+			<hr />
 		</nav>
 	);
 }
