864750ac8db5a302f9d10985d955657d2f72aaa7
Mert Toprak <mert.tprk@gmail.com>
2023-11-23 11:25:02 +0100

    switch API from local to online

--- src/api.js
+++ src/api.js
@@ -1,22 +1,21 @@
-const online = 'https://bootcamp-json-server-backend.adaptable.app'
-const local = 'http://127.0.0.1:5005'
-
+const online = 'https://bootcamp-json-server-backend.adaptable.app';
+const local = 'http://127.0.0.1:5005';
 
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: local,
+	baseURL: online,
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const accessToken = localStorage.getItem('accessToken')
-        if (accessToken) {
-            config.headers.Authorization = `Bearer ${accessToken}`
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-)
+	(config) => {
+		const accessToken = localStorage.getItem('accessToken');
+		if (accessToken) {
+			config.headers.Authorization = `Bearer ${accessToken}`;
+		}
+		return config;
+	},
+	(error) => Promise.reject(error)
+);
 
 export default api;
