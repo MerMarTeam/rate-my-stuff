9d8a1722bc6f8868613ba761f6f0f0bed463ba71
Mert Toprak <mert.tprk@gmail.com>
2023-11-24 10:16:16 +0100

    changed css and added list numbers to top list + moved info to footer

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,139 +1,167 @@
-import { useEffect, useState } from "react";
-import api from './../api'
+import { useEffect, useState } from 'react';
+import api from './../api';
 
 function Rating(props) {
-    let [givenRating, setGivenRating] = useState(0)
-    let [averageRating, setAverageRating] = useState(0)
+	let [givenRating, setGivenRating] = useState(0);
+	let [averageRating, setAverageRating] = useState(0);
 
-    const [star1Hovered, setStar1Hovered] = useState(false)
-    const [star2Hovered, setStar2Hovered] = useState(false)
-    const [star3Hovered, setStar3Hovered] = useState(false)
-    const [star4Hovered, setStar4Hovered] = useState(false)
-    const [star5Hovered, setStar5Hovered] = useState(false)
+	const [star1Hovered, setStar1Hovered] = useState(false);
+	const [star2Hovered, setStar2Hovered] = useState(false);
+	const [star3Hovered, setStar3Hovered] = useState(false);
+	const [star4Hovered, setStar4Hovered] = useState(false);
+	const [star5Hovered, setStar5Hovered] = useState(false);
 
-    const toggleHover1 = () => setStar1Hovered(!star1Hovered);
-    const toggleHover2 = () => setStar2Hovered(!star2Hovered);
-    const toggleHover3 = () => setStar3Hovered(!star3Hovered);
-    const toggleHover4 = () => setStar4Hovered(!star4Hovered);
-    const toggleHover5 = () => setStar5Hovered(!star5Hovered);
+	const toggleHover1 = () => setStar1Hovered(!star1Hovered);
+	const toggleHover2 = () => setStar2Hovered(!star2Hovered);
+	const toggleHover3 = () => setStar3Hovered(!star3Hovered);
+	const toggleHover4 = () => setStar4Hovered(!star4Hovered);
+	const toggleHover5 = () => setStar5Hovered(!star5Hovered);
 
-    useEffect(() => {
-        if (props.post.ratings) {
-            calculateAverageRating();
-        }
-    }, [givenRating]);
+	useEffect(() => {
+		if (props.post.ratings) {
+			calculateAverageRating();
+		}
+	}, [givenRating]);
 
-    function calculateAverageRating() {
-        if (props.post.ratings) {
-            let sum = 0;
+	function calculateAverageRating() {
+		if (props.post.ratings) {
+			let sum = 0;
 
-            if (givenRating > 0) sum = givenRating;
-            for (let i = 0; i < props.post.ratings.length; i++) {
-                sum += props.post.ratings[i].starsCount
-            }
+			if (givenRating > 0) sum = givenRating;
+			for (let i = 0; i < props.post.ratings.length; i++) {
+				sum += props.post.ratings[i].starsCount;
+			}
 
-            let average = 0;
-            // If the user provided a new rating, we shall divide by one more
-            if (givenRating > 0) {
-                average = Math.ceil(sum / (props.post.ratings.length + 1));
-            } else {
-                average = Math.ceil(sum / props.post.ratings.length)
-            }
-            setAverageRating(average);
-        }
-    }
+			let average = 0;
+			// If the user provided a new rating, we shall divide by one more
+			if (givenRating > 0) {
+				average = Math.ceil(sum / (props.post.ratings.length + 1));
+			} else {
+				average = Math.ceil(sum / props.post.ratings.length);
+			}
+			setAverageRating(average);
+		}
+	}
 
-    function handleStarClick(event) {
-        let userRating = 0;
+	function handleStarClick(event) {
+		let userRating = 0;
 
-        for (let i = 1; i <= 5; i++) {
-            if (event.target.id == `rating-${i}`) userRating = i;
-        }
+		for (let i = 1; i <= 5; i++) {
+			if (event.target.id == `rating-${i}`) userRating = i;
+		}
 
-        setGivenRating(userRating);
+		setGivenRating(userRating);
 
-        let newRatingObject = {
-            starsCount: userRating,
-            postId: props.post.id
-        }
+		let newRatingObject = {
+			starsCount: userRating,
+			postId: props.post.id,
+		};
 
-        props.postNewRating(newRatingObject);
+		props.postNewRating(newRatingObject);
 
-        if (props.calculateTopFivePosts) props.calculateTopFivePosts();
-    }
+		if (props.calculateTopFivePosts) props.calculateTopFivePosts();
+	}
 
-    function toggleCorrectHovers(id) {
-        switch (id) {
-            case 'rating-1':
-                toggleHover1();
-                break;
-            case 'rating-2':
-                toggleHover1();
-                toggleHover2();
-                break;
-            case 'rating-3':
-                toggleHover1();
-                toggleHover2();
-                toggleHover3();
-                break;
-            case 'rating-4':
-                toggleHover1();
-                toggleHover2();
-                toggleHover3();
-                toggleHover4();
-                break;
-            case 'rating-5':
-                toggleHover1();
-                toggleHover2();
-                toggleHover3();
-                toggleHover4();
-                toggleHover5();
-                break;
-            default:
-                break;
-        }
-    }
+	function toggleCorrectHovers(id) {
+		switch (id) {
+			case 'rating-1':
+				toggleHover1();
+				break;
+			case 'rating-2':
+				toggleHover1();
+				toggleHover2();
+				break;
+			case 'rating-3':
+				toggleHover1();
+				toggleHover2();
+				toggleHover3();
+				break;
+			case 'rating-4':
+				toggleHover1();
+				toggleHover2();
+				toggleHover3();
+				toggleHover4();
+				break;
+			case 'rating-5':
+				toggleHover1();
+				toggleHover2();
+				toggleHover3();
+				toggleHover4();
+				toggleHover5();
+				break;
+			default:
+				break;
+		}
+	}
 
-    function handleStarHover(event) {
-        toggleCorrectHovers(event.target.id)
-    }
+	function handleStarHover(event) {
+		toggleCorrectHovers(event.target.id);
+	}
 
-    const starStyle = {
-        marginLeft: '20px',
-        marginRight: '20px',
-    }
+	const starStyle = {
+		marginLeft: '20px',
+		marginRight: '20px',
+	};
 
-    return (
+	return (
+		<div className="post-rating">
+			<p>
+				Average rating:{' '}
+				{averageRating
+					? averageRating +
+					  ' ' +
+					  '(' +
+					  'out of' +
+					  ' ' +
+					  props.post.ratings.length +
+					  ' ratings)'
+					: 'no rating yet'}
+			</p>
 
-        <div className='post-rating'>
-            <p>Average rating: {
-                averageRating ? averageRating + " " + "(" + "out of" + " " + props.post.ratings.length + ")" : "no rating yet"
-            }</p>
+			<i
+				id="rating-1"
+				style={starStyle}
+				onClick={handleStarClick}
+				className={star1Hovered ? 'bi bi-star-fill fa-2x' : 'bi bi-star fa-2x'}
+				onMouseEnter={handleStarHover}
+				onMouseLeave={handleStarHover}
+			></i>
+			<i
+				id="rating-2"
+				style={starStyle}
+				onClick={handleStarClick}
+				className={star2Hovered ? 'bi bi-star-fill fa-2x' : 'bi bi-star fa-2x'}
+				onMouseEnter={handleStarHover}
+				onMouseLeave={handleStarHover}
+			></i>
+			<i
+				id="rating-3"
+				style={starStyle}
+				onClick={handleStarClick}
+				className={star3Hovered ? 'bi bi-star-fill fa-2x' : 'bi bi-star fa-2x'}
+				onMouseEnter={handleStarHover}
+				onMouseLeave={handleStarHover}
+			></i>
+			<i
+				id="rating-4"
+				style={starStyle}
+				onClick={handleStarClick}
+				className={star4Hovered ? 'bi bi-star-fill fa-2x' : 'bi bi-star fa-2x'}
+				onMouseEnter={handleStarHover}
+				onMouseLeave={handleStarHover}
+			></i>
+			<i
+				id="rating-5"
+				style={starStyle}
+				onClick={handleStarClick}
+				className={star5Hovered ? 'bi bi-star-fill fa-2x' : 'bi bi-star fa-2x'}
+				onMouseEnter={handleStarHover}
+				onMouseLeave={handleStarHover}
+			></i>
 
-            <i id="rating-1" style={starStyle} onClick={handleStarClick} className={star1Hovered ? "bi bi-star-fill fa-2x" : "bi bi-star fa-2x"}
-                onMouseEnter={handleStarHover}
-                onMouseLeave={handleStarHover}>
-            </i>
-            <i id="rating-2" style={starStyle} onClick={handleStarClick} className={star2Hovered ? "bi bi-star-fill fa-2x" : "bi bi-star fa-2x"}
-                onMouseEnter={handleStarHover}
-                onMouseLeave={handleStarHover}>
-            </i>
-            <i id="rating-3" style={starStyle} onClick={handleStarClick} className={star3Hovered ? "bi bi-star-fill fa-2x" : "bi bi-star fa-2x"}
-                onMouseEnter={handleStarHover}
-                onMouseLeave={handleStarHover}>
-            </i>
-            <i id="rating-4" style={starStyle} onClick={handleStarClick} className={star4Hovered ? "bi bi-star-fill fa-2x" : "bi bi-star fa-2x"}
-                onMouseEnter={handleStarHover}
-                onMouseLeave={handleStarHover}>
-            </i>
-            <i id="rating-5" style={starStyle} onClick={handleStarClick} className={star5Hovered ? "bi bi-star-fill fa-2x" : "bi bi-star fa-2x"}
-                onMouseEnter={handleStarHover}
-                onMouseLeave={handleStarHover}>
-            </i>
-
-            <p>Given rating: {givenRating}</p>
-        </div>
-    )
+			<p>Given rating: {givenRating}</p>
+		</div>
+	);
 }
 
 export default Rating;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,28 +5,34 @@ import Rating from '../components/Rating';
 
 function HomePage() {
 	const [postsArray, setPostsArray] = useState([]);
-	const [topFive, setTopFive] = useState([])
+	const [topFive, setTopFive] = useState([]);
 
 	useEffect(() => {
 		getPostsFromApi();
 	}, []);
 
 	useEffect(() => {
-		calculateTopFivePosts()
-	}, [postsArray])
+		calculateTopFivePosts();
+	}, [postsArray]);
 
 	function calculateTopFivePosts() {
-		let postsIdsWithAverages = []
+		let postsIdsWithAverages = [];
 		if (postsArray.length > 0) {
 			postsArray.forEach((post) => {
 				let sum = 0;
 				for (let i = 0; i < post.ratings.length; i++) {
-					sum += post.ratings[i].starsCount
+					sum += post.ratings[i].starsCount;
 				}
-				let average = Math.ceil(sum / post.ratings.length)
-				postsIdsWithAverages.push({ id: post.id, title: post.title, average: average })
-			})
-			let tempFive = [...postsIdsWithAverages].sort((a, b) => b.average - a.average).slice(0, 5)
+				let average = Math.ceil(sum / post.ratings.length);
+				postsIdsWithAverages.push({
+					id: post.id,
+					title: post.title,
+					average: average,
+				});
+			});
+			let tempFive = [...postsIdsWithAverages]
+				.sort((a, b) => b.average - a.average)
+				.slice(0, 5);
 			setTopFive(tempFive);
 		}
 	}
@@ -60,98 +66,115 @@ function HomePage() {
 	}
 
 	return (
-		<div className="parent-div">
-			<div className="homepage">
-				<section>
-					<h3>Who are we?</h3>
-					<p>
-						We are Marat & Mert and this is our second project for Ironhack
-						Fullstack Web Development Course!
-					</p>
-				</section>
-
-				<section>
-					<h3>What is Rate My Stuff?</h3>
-					<p>
-						Welcome to RMS, your go-to platform for sharing and celebrating the
-						things that matter to you! Upload your favorite images and let the
-						community weigh in with votes and comments. Whether it's your latest
-						DIY project, a stunning travel photo, or anything else you're proud
-						of, "Rate My Stuff" is the place to showcase your creations and
-						gather feedback from a supportive community. Join us in fostering a
-						vibrant space for creativity, inspiration, and friendly
-						interactions. Start uploading, voting, and engaging today!
-					</p>
-				</section>
-
-				<div className="title">
-					<h1>Stuffs to Rate</h1>
-				</div>
-				{[...postsArray].reverse().map((post, i) => {
-					return (
-						<div key={post.id} className="post">
-							<Link to={`/posts/${post.id}`}>
-								<h2>{post.title}</h2>
-								<img src={post.image} alt="" />
-							</Link>
-
-							<dl className="description">{post.description}</dl>
-
-							<Rating post={post} postNewRating={postNewRating} calculateTopFivePosts={calculateTopFivePosts} />
-
-							<div className="comments-count">
+		<>
+			<div className="parent-div">
+				<div className="homepage">
+					<div className="title">
+						<h1>Stuffs to Rate</h1>
+					</div>
+					{[...postsArray].reverse().map((post, i) => {
+						return (
+							<div key={post.id} className="post">
 								<Link to={`/posts/${post.id}`}>
-									<i
-										className="fa-regular fa-comments fa-xl"
-										style={{ color: '#ffffff' }}
-										onMouseEnter={(event) => {
-											event.target.className =
-												'fa-regular fa-comments fa-xl fa-beat';
-										}}
-										onMouseLeave={(event) => {
-											event.target.className = 'fa-regular fa-comments fa-xl';
-										}}
-									></i>
-									<h4
-										style={{
-											display: 'inline',
-											textDecoration: 'none',
-											color: 'white',
-											marginLeft: '10px',
-										}}
-									>
-										{post.comments.length}
-									</h4>
+									<h2>{post.title}</h2>
+									<img src={post.image} alt="" />
 								</Link>
-							</div>
-						</div>
-					);
-				})}
-				<br />
-				<Link to="/create">
-					<button id="create-post-button" className="btn btn-primary btn-circle">
-						<p>+</p>
-					</button>
-				</Link>
 
-			</div>
-			<div className="side-panel">
-				<section>
-					<h3>Top 5 Posts</h3>
-					<ul className='top-five-list'>
-						{topFive && topFive.map((post) => {
-							return (
-								<li key={post.id} className=''>
+								<dl className="description">{post.description}</dl>
+
+								<Rating
+									post={post}
+									postNewRating={postNewRating}
+									calculateTopFivePosts={calculateTopFivePosts}
+								/>
+
+								<div className="comments-count">
 									<Link to={`/posts/${post.id}`}>
-										{post.title.length > 20 ? post.title.slice(0, 20) + "..." : post.title}
+										<i
+											className="fa-regular fa-comments fa-xl"
+											style={{ color: '#ffffff' }}
+											onMouseEnter={(event) => {
+												event.target.className =
+													'fa-regular fa-comments fa-xl fa-beat';
+											}}
+											onMouseLeave={(event) => {
+												event.target.className = 'fa-regular fa-comments fa-xl';
+											}}
+										></i>
+										<h4
+											style={{
+												display: 'inline',
+												textDecoration: 'none',
+												color: 'white',
+												marginLeft: '10px',
+											}}
+										>
+											{post.comments.length}
+										</h4>
 									</Link>
-								</li>
-							)
-						})}
-					</ul>
-				</section>
+								</div>
+							</div>
+						);
+					})}
+					<br />
+					<Link className="create-post-link" to="/create">
+						<button
+							id="create-post-button"
+							className="btn btn-primary btn-circle"
+						>
+							<p>+</p>
+						</button>
+					</Link>
+				</div>
+				<div className="side-panel">
+					<section>
+						<h3>Top 5 Posts</h3>
+						<hr />
+						<ul className="top-five-list">
+							{topFive &&
+								topFive.map((post, i) => {
+									return (
+										<li key={post.id} className="top-element">
+											<Link to={`/posts/${post.id}`}>
+												{i + 1}.{' '}
+												{post.title.length > 20
+													? post.title.slice(0, 20) + '...'
+													: post.title}
+											</Link>
+										</li>
+									);
+								})}
+						</ul>
+					</section>
+				</div>
 			</div>
-		</div >
+			<div className="footer-div">
+				<footer className="parent-footer">
+					<section className="who-are-we">
+						<h3>Who are we?</h3>
+						<p>
+							We are Marat & Mert and this is our second project for Ironhack
+							Fullstack Web Development Course!
+						</p>
+					</section>
+
+					<section className="what-is">
+						<h3>What is Rate My Stuff?</h3>
+						<p>
+							Welcome to RMS, your go-to platform for sharing and celebrating
+							the things that matter to you! Upload your favorite images and let
+							the community weigh in with votes and comments. Whether it's your
+							latest DIY project, a stunning travel photo, or anything else
+							you're proud of, "Rate My Stuff" is the place to showcase your
+							creations and gather feedback from a supportive community. Join us
+							in fostering a vibrant space for creativity, inspiration, and
+							friendly interactions. Start uploading, voting, and engaging
+							today!
+						</p>
+					</section>
+				</footer>
+			</div>
+		</>
 	);
 }
 
